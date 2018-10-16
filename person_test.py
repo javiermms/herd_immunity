a3eb816e9f5bbe0f4d10968ec7cff1e2732db128
Javier Mendoza <javier.mendoza@students.makeschool.com>
2018-10-16 16:28:22 -0700

    Initial test commit

--- person_test.py
+++ person_test.py
@@ -1,9 +1,24 @@
 import pytest
 from person import Person 
 
+
 def test_instance_functionality():
 	person = Person(12, False, None)
+
 	assert person._id == 12
 	assert person.is_alive is True
 	assert person.is_vaccinated is False
 	assert person.infected is None
+
+def test_did_survive_method():
+	person_infected = Person(20, False, True)
+
+	assert person_infected.did_survive_infection(12) is False
+	assert person_infected.is_alive is False
+	
+
+	
+
+
+
+	
